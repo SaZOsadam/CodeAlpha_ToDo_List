7431f8fe781623b567be312ea1cc4f219ab6bd33
SaZOsadam <soladiran25@gmail.com>
2024-04-15 09:22:21 +0100

    commit all update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let inputBox = document.getElementById('inputBox');
 let listContainer = document.getElementById('listContainer');
 
-
+//To add in Task and to notified if input box is empty  
 function addingTask() {
-    if (inputBox.value === " "){
+    if (inputBox.value === ''){
             alert("No task inputed yet!");
     } else {
-        const li = document.createElement("li");
+        const li = document.createElement("LI");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        const span = document.createElement("span");
+        const span = document.createElement("SPAN");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
@@ -17,6 +17,7 @@ function addingTask() {
     saveData();
 }
 
+// To check Task that has be done or delete Task
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === 'LI'){
         e.target.classList.toggle("mark-checked");
@@ -27,11 +28,11 @@ listContainer.addEventListener("click", function(e){
         saveData();
     }
 }, false);
-
+// To save Task in local storage
 function saveData(){
     localStorage.setItem("saveData", listContainer.innerHTML);
 }
-
+//To save Task in browser
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("savedData");
 }
